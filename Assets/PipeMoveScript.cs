fe94f2cb67fe8a6a1b15cdc23708c0136740daf9
Hanaa <anna48691880@gmail.com>
2025-06-26 19:46:41 +0100

    sauvgarde des modifs des scripts

--- Assets/PipeMoveScript.cs
+++ Assets/PipeMoveScript.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BirdScript : MonoBehaviour
+public class PipeMoveScript : MonoBehaviour
 {
-    public Rigidbody2D myRigidbody;
-    public float flapStrength;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +13,6 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            myRigidbody.velocity = Vector2.up * flapStrength;
-        }
+        
     }
 }
