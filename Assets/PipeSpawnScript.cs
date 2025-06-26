ad4063fc084c6c2d35dd8ff727340644bff4e13f
Hanaa <anna48691880@gmail.com>
2025-06-26 20:23:15 +0100

    sauvgarde des modifs des scripts vol3

--- Assets/PipeSpawnScript.cs
+++ Assets/PipeSpawnScript.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 public class PipeSpawnScript : MonoBehaviour
 {
     public GameObject pipe;
+    public float spawnRate = 2;
+    private float timer = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnPipe();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer < spawnRate)
+        {
+            timer = timer + Time.deltaTime;
+        }
+        else
+        {
+            spawnPipe();
+            timer = 0;
+        }
+
+    }
+
+    void spawnPipe()
+    {
+        Instantiate(pipe, transform.position, transform.rotation);
     }
 }
